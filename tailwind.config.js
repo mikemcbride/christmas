9d5eab3d42849b3fadc77aebb44be2cf9166d215
Mike McBride <mike.mcbride@hey.com>
2021-12-23 18:13:47 -0600

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,44 @@
 module.exports = {
-  purge: [ "./src/_includes/**/*.njk", "./src/*.html" ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/_includes/**/*.njk", "./src/**/*.html", "./src/**/*.njk"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': [
+                    'Inter',
+                    'system-ui',
+                    'BlinkMacSystemFont',
+                    '-apple-system',
+                    'Segoe UI',
+                    'Roboto',
+                    'Oxygen',
+                    'Ubuntu',
+                    'Cantarell',
+                    'Fira Sans',
+                    'Droid Sans',
+                    'Helvetica Neue',
+                    'sans-serif',
+                ],
+                'display': [
+                    'Notulen Display',
+                    'Atkinson Hyperlegible',
+                    'system-ui',
+                    'BlinkMacSystemFont',
+                    '-apple-system',
+                    'Segoe UI',
+                    'Roboto',
+                    'Oxygen',
+                    'Ubuntu',
+                    'Cantarell',
+                    'Fira Sans',
+                    'Droid Sans',
+                    'Helvetica Neue',
+                    'sans-serif',
+                ],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/typography')],
 }
