01a58dea28891242c3ce4535a00caecfa717750a
Mike McBride <mike.mcbride@hey.com>
2024-12-10 08:04:55 -0600

    format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), vue()]
+  integrations: [mdx(), tailwind(), vue()],
 });
